cda17d9e436d2a6c092e748128968a83e1c4a861
Zein Shhadi <zeinshhadi@hotmail.com>
2023-11-16 12:45:35 +0200

    added footer

--- js/index.js
+++ js/index.js
@@ -2,32 +2,32 @@ let colorOverlay = document.querySelector(".hero-left .color-overlay");
 let typedInput = document.querySelector(".auto-input");
 let heroDesc = document.querySelector(".hero-left-desc");
 let typed = new Typed(".auto-input", {
-   strings: ["SOFTWARE ENGINEER?", "DATA ENGINEER?", "UI/UX DESIGNER?"],
-   typeSpeed: 70,
-   backSpeed: 70,
-   loop: true,
-   loopCount: false,
-   cursorChar: "_",
-   preStringTyped: function (arrayPos, self) {
-      switch (arrayPos) {
-         case 0:
-            colorOverlay.style.background = "var(--overlay-green)";
-            typedInput.style.color = "var(--black)";
-            self.cursor.style.color = "var(--black)";
-            heroDesc.style.color = "var(--black)";
+  strings: ["SOFTWARE ENGINEER?", "DATA ENGINEER?", "UI/UX DESIGNER?"],
+  typeSpeed: 70,
+  backSpeed: 70,
+  loop: true,
+  loopCount: false,
+  cursorChar: "_",
+  preStringTyped: function (arrayPos, self) {
+    switch (arrayPos) {
+      case 0:
+        colorOverlay.style.background = "var(--overlay-green)";
+        typedInput.style.color = "var(--black)";
+        self.cursor.style.color = "var(--black)";
+        heroDesc.style.color = "var(--black)";
 
-            break;
-         case 1:
-            colorOverlay.style.background = "var(--purple-overlay)";
-            typedInput.style.color = "var(--white)";
-            self.cursor.style.color = "var(--white)";
-            heroDesc.style.color = "var(--white)";
-            break;
-         case 2:
-            colorOverlay.style.background = "var(--pink-overlay)";
-            break;
-      }
-   },
+        break;
+      case 1:
+        colorOverlay.style.background = "var(--purple-overlay)";
+        typedInput.style.color = "var(--white)";
+        self.cursor.style.color = "var(--white)";
+        heroDesc.style.color = "var(--white)";
+        break;
+      case 2:
+        colorOverlay.style.background = "var(--pink-overlay)";
+        break;
+    }
+  },
 });
 
 const navButton = document.querySelector("#nav-btn").addEventListener("click", displayMenu);
@@ -36,85 +36,78 @@ const menuIcon = document.querySelector("#nav-btn");
 const mainSection = document.querySelector(".main-section");
 
 function displayMenu() {
-   if (navMenu.style.display === "block") {
-      navMenu.style.display = "none";
-      menuIcon.innerHTML = '<i class="fa fa-bars"></i>';
-   } else {
-      navMenu.style.display = "block";
-      menuIcon.innerHTML = '<i class="fa fa-times"></i>';
-   }
+  if (navMenu.style.display === "block") {
+    navMenu.style.display = "none";
+    menuIcon.innerHTML = '<i class="fa fa-bars"></i>';
+  } else {
+    navMenu.style.display = "block";
+    menuIcon.innerHTML = '<i class="fa fa-times"></i>';
+  }
 }
 
 const tabs = document.querySelectorAll("[data-tab-target]");
 const tabContents = document.querySelectorAll("[data-tab-content]");
-
+const tabSection = document.getElementById("tabs-section");
+const tabSpan = document.getElementById("tab-span");
 tabs.forEach((tab) => {
-   tab.addEventListener("click", () => {
-      const target = document.querySelector(tab.dataset.tabTarget);
-      tabContents.forEach((tabContent) => {
-         tabContent.classList.remove("active");
-      });
-      tabs.forEach((tab) => {
-         tab.classList.remove("active");
-      });
-      tab.classList.add("active");
-      target.classList.add("active");
-   });
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.tabTarget);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("active");
+    });
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tab.classList.add("active");
+    target.classList.add("active");
+    const sectionColor = tab.dataset.sectionColor;
+    tabSection.style.backgroundColor = sectionColor;
+  });
 });
 
 let currentSlide = 0;
 const slides = document.querySelectorAll(".testimonial-slide");
 const dots = document.querySelectorAll(".dot");
 
-function showSlide(index) {
-   slides.forEach((slide) => {
-      slide.style.display = "none";
-   });
+function showSlide(event) {
+  slides.forEach((slide) => {
+    slide.style.display = "none";
+  });
 
-   dots.forEach((dot) => {
-      dot.classList.remove("active");
-   });
+  dots.forEach((dot) => {
+    dot.classList.remove("active");
+  });
 
-   slides[index].style.display = "block";
-   dots[index].classList.add("active");
+  slides[event].style.display = "block";
+  dots[event].classList.add("active");
 }
 
 function nextSlide() {
-   currentSlide = (currentSlide + 1) % slides.length;
-   showSlide(currentSlide);
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
 }
 
 function prevSlide() {
-   currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-   showSlide(currentSlide);
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
 }
 
-function setSlide(index) {
-   currentSlide = index;
-   showSlide(currentSlide);
+function setSlide(event) {
+  currentSlide = event;
+  showSlide(currentSlide);
 }
 
 setInterval(nextSlide, 5000);
 
 dots.forEach((dot, index) => {
-   dot.addEventListener("click", () => {
-      setSlide(index);
-   });
+  dot.addEventListener("click", () => {
+    setSlide(index);
+  });
 });
 
 const faqItems = document.querySelectorAll(".faq-item");
-const viewAll = document.getElementById("view-all");
-const allFaqs = document.getElementById("all-faqs");
-
 faqItems.forEach((item) => {
-   const question = item.querySelector(".faq-question");
-   const answer = item.querySelector(".faq-answer");
-
-   question.addEventListener("click", () => {
-      item.classList.toggle("open");
-   });
-});
-
-viewAll.addEventListener("click", () => {
-   allFaqs.classList.toggle("open");
+  item.addEventListener("click", () => {
+    item.classList.toggle("open");
+  });
 });
